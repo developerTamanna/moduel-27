eb55be96596e3c47fcc1693fc704912eba955640
developerTamanna <astamanna57@gmail.com>
2025-02-25 19:27:50 +0600

    window add

--- js/login.js
+++ js/login.js
@@ -1,22 +1,17 @@
 document.getElementById('login-btn')
 .addEventListener('click', function(event){
     event.preventDefault();
-    // console.log('hello')
     const accountNumber = document.getElementById('account-number').value;
-    // console.log(accountNumber)
     const pinSet = document.getElementById('pin-number').value;
-    // console.log(pinSet)
-    if(accountNumber.length===11){
-        // console.log('yes right number')
-        window.location.href="./main.html"
-        if(parseInt(pinSet)===1234){
-            console.log('right pin')
+
+    if (accountNumber.length === 11) {
+        if (parseInt(pinSet) === 1234) {
+            console.log('right pin');
+            window.location.href = "./main.html"; // Page redirect
+        } else {
+            alert('wrong pin');
         }
-        else{
-            alert('wrong pin')
-        }
+    } else {
+        alert('need valid account number');
     }
-    else{
-        alert('need valid acount number')
-    }
-})
+});
