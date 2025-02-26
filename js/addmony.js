75f5c106771526ac1ecc901a706fc86c8698467a
developerTamanna <astamanna57@gmail.com>
2025-02-26 11:54:37 +0600

    ad mony

--- js/addmony.js
+++ js/addmony.js
@@ -1,23 +1,40 @@
-document.getElementById('add-mony')
-.addEventListener('click', function(event){
+document.getElementById('add-mony-1').addEventListener('click', function(event){
 
 event.preventDefault();
-const amount = document.getElementById('amount').value;
+const amount = document.getElementById('amount-add').value;
 const converTedAmount = parseFloat(amount)
 // console.log(amount);
 const pin = document.getElementById('pin-number').value;
 const convertedPin = parseInt(pin);
-const mainBalance = document.getElementById('main-balance').innerText;
+const mainBalance = document.getElementById('main-balance-add');
 // console.log(typeof mainBalance);
-const covertMainBalance = parseFloat(mainBalance);
+const covertMainBalance = parseFloat(mainBalance.innerText);
+const acountNumber = document.getElementById('account-number').value;
 
+
+  if(acountNumber.length === 11){
+   // console.log('ok')
    if(convertedPin === 1234){
-    // console.log('ad to mony');
-    const sum = covertMainBalance + converTedAmount;
-    // console.log(sum);
-    document.getElementById('main-balance').innerText = sum;
+      // console.log('ad to money');
+      console.log(converTedAmount, covertMainBalance)
+      const sum = converTedAmount + covertMainBalance;
+      console.log(sum)
+      mainBalance.innerText = sum;
    }
    else{
-    console.log('Mony is not add , wrong pin')
+      alert('wrong pin');
    }
+  }
+  else{
+   console.log('Mony is not add , wrong pin')
+  }
+   // if(convertedPin === 1234){
+   //  // console.log('ad to mony');
+   //  const sum = converTedAmount + covertMainBalance ;
+   // //  console.log(sum);
+   // //  document.getElementById('main-balance').innerText = sum;
+   // }
+   // else{
+   //  console.log('Mony is not add , wrong pin')
+   // }
 })
--- js/cashout.js
+++ js/cashout.js
@@ -1,16 +1,15 @@
-document.getElementById('cashout-btn')
-.addEventListener('click', function(event){
+document.getElementById('cashout-btn').addEventListener('click', function(event){
 event.preventDefault();
  const pin = document.getElementById('cashout-pin-number').value;
  const convertedPin = parseInt(pin);
  const amount = document.getElementById('cashout-amount').value; 
  convertedAmount = parseFloat(amount);
- const mainBalance = document.getElementById('main-balance').innerText;
+ const mainBalance = document.getElementById('main-balance-add').innerText;
  convertedMainBalance = parseFloat(mainBalance);
  if(convertedPin === 1234){
             const sum = convertedMainBalance - convertedAmount;
             // console.log(sum)
-            document.getElementById('main-balance').innerText = sum;
+            document.getElementById('main-balance-add').innerText = sum;
  }
  else{
     alert('incorrect pin')
